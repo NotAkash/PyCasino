84008d6a59720818fa36d86f98e6c8d4d3305f4f
Akash Singh <singhakash0412@gmail.com>
2017-11-08 21:20:01 +0530

    Delete PyCasino.py

--- PyCasino.py
+++
@@ -1,5 +0,0 @@
-
- #TODO find a way to somehow be able to hide all the source code except this file where I,
- #TODO  input casino.py and then when a .pyc file is created I move everything else away and only use that so this becomes every __init__ file ever
-
-from casino import *
