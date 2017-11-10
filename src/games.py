8bad627b83132e69c933cbcbc4a9099e1e648a73
Not Akash <singhakash0412@gmail.com>
2017-11-10 22:20:50 +0530

    Main Commit

--- src/games.py
+++ src/games.py
@@ -243,64 +243,7 @@ def roulette(choice,gamble):
 
 
 def usr():
-    while True: #Keep repeating until user is logged in
-        try:
-            print("Welcome To The Casino")
-            user=input("Are You An 'Existing' User Or A 'New' User?: ")
-        except ValueError:                      #Error Hashes
-            print("No Value Entered")
-            continue
-        except KeyboardInterrupt:
-                print("Force Exitting Game.",end='')
-                print(".",end='')
-                print(".",end='')
-                exit()
-        if (user.lower() != "existing" and user.lower() != "new"):  # Repeat till user answer is Valid
-            print("Please Try Again")
-            continue
-        elif (user.lower() == "existing"):  # If existing
-            while True:
-                try:
-                    name = input("Welcome Back! What Is Your Name?: ")  # Get User-Name
-                except KeyboardInterrupt:
-                    print("Force Exiting Game.", end='')
-                    print(".", end='')
-                    print(".", end='')
-                    exit()
-                if name == '':
-                    print("Invalid Value")
-                    continue
-                else:
-                    break
-            if player_cont(name) >= 0 and player_cont(name) != False:  # Unless False Is Returned Or Balance Is 0
-                print("Welcome Back", name, "Current Balance: ", player_cont(name))
-                money = player_cont(name)  # Show User Value
-                break
-            else:
-                print("User Does Not Exist, Try Again")
-                continue  # Else, Repeat Till Validated
-        elif (user.lower() == "new"):  # If New,
-            print("Welcome To The Casino!")
-            while True:
-                try:
-                    name = input("Welcome! What Is Your Name?: ")  # Get User-Name
-                except KeyboardInterrupt:
-                    print("Force Exiting Game.", end='')
-                    print(".", end='')
-                    print(".", end='')
-                    exit()
-                if name == '':
-                    print("Invalid Value, Try Again")
-                    continue
-                else:
-                    break
-            if (player_init(name) != False):  # Initialise Name Unless Flase Returned
-                print("Welcome!", name, "Starting Balance: ", player_cont(name))
-                money = player_cont(name)  # Get Account Balance
-                break
-            else:
-                print("User Exists, Try Again")
-                continue
+    name = start_game()
     return(name)
 
 
