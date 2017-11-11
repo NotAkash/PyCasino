9c836252d91830f0587cc1bad3bf10b4ffaf2a32
Not Akash <singhakash0412@gmail.com>
2017-11-11 20:52:13 +0530

    Play File Patch

--- src/play.py
+++ src/play.py
@@ -1,135 +1,107 @@
-# Database For Players
-def offset():
-    # used to find the last line
-    while True:
-        try:
-            player= open("src/plist.txt","r+")              #This algorithm validates where the file is opening gplist.txt from
-        except FileNotFoundError:                           #if it's being opened from parent, then open from "src/plist.txt"
-            player= open("plist.txt","r+")                       #if being opened from sub then open from "plist.txt"
-            break
-        else:
-            break
-    offset = 0
-    for line in player:
-        offset += len(line)
-    player.close()
-    return (offset)
+#New CSV Supporting Database, To Compensate For The Senseless Newline (\r\n) On Windows
+#Reworked The Entire Database On .csv (Comma Seperated Values)
+#Import Files
 
 
-def offsetline():
-    # used to find all new lines
-    while True:
-        try:
-            player= open("src/plist.txt","r+")              #This algorithm validates where the file is opening gplist.txt from
-        except FileNotFoundError:                           #if it's being opened from parent, then open from "src/plist.txt"
-            player= open("plist.txt","r+")                       #if being opened from sub then open from "plist.txt"
-            break
-        else:
-            break
-    # This algorithm finds the last line of the txt file
-    offsetline = []
-    offset = 0
-    player.seek(0)
-    for line in player:
-        offsetline.append(offset)
-        offset += len(line)
-    player.close()
-    return (offsetline)
+import csv
+
+#Open files
+def openfile():
+    try:
+        player = open('src/plist.csv',"r+", newline='')  # This algorithm validates where the file is opening plist.csv from
+    except FileNotFoundError:                        # if it's being opened from parent, then open from "src/plist.csv"
+        player = open("plist.csv","r+", newline='')
+    return(player)
 
 
-def pseek():
-    # used to go to last line of file
-    while True:
-        try:
-            player= open("src/plist.txt","r+")              #This algorithm validates where the file is opening gplist.txt from
-        except FileNotFoundError:                           #if it's being opened from parent, then open from "src/plist.txt"
-            player= open("plist.txt","r+")                       #if being opened from sub then open from "plist.txt"
-            break
-        else:
-            break
-    ofset = offset()
-    if (ofset > 1):
-        player.seek(ofset)
-        pseek = player.seek(ofset)
-    else:
-        player.seek(1)
-        pseek = player.seek(1)
-    player.close()
-    return (pseek)
+def writeinfile():
+    try:
+        player = open('src/plist.csv',"w", newline='')  # This algorithm validates where the file is opening plist.csv from
+    except FileNotFoundError:                        # if it's being opened from parent, then open from "src/plist.csv"
+        player = open("plist.csv","w", newline='')
+    return(player)
 
 
 def player_init(name):
-    # print(player.read()) === read all users
-    # Used to initialise a new unexisting player
-    while True:
-        try:
-            player= open("src/plist.txt","r+")              #This algorithm validates where the file is opening gplist.txt from
-        except FileNotFoundError:                           #if it's being opened from parent, then open from "src/plist.txt"
-            player= open("plist.txt","r+")                       #if being opened from sub then open from "plist.txt"
-            break
-        else:
-            break
-    player.seek(0)
-    for i in offsetline():
-        player.seek(i)
-        if (player.readline() == (name + "\n")):
+    player= openfile()
+    readfile = csv.reader(player)
+    rows=[]
+    for row in readfile:
+        rows.append(row)
+
+    for row in readfile:
+        rows.append(row)
+        if row==name:
             player.close()
             return False
-    toseek = pseek()
-    player.seek(toseek)
-    player.write(("\n\n%s\n500.00\n") % name)  # this format organizes names
-    player.seek(0)
+
     player.close()
+    player= writeinfile()
+    writer= csv.writer(player)
+
+    writer.writerows(rows)
+    names=[]
+    names.append(name)
+    names.append("500")
+    name=names
+    writer.writerow([""])
+    writer.writerow(name)
+
+    return
 
 
 def player_cont(name):
-    name = str(name)
-    # Returns data for an existing player
-    while True:
-        try:
-            player= open("src/plist.txt","r+")              #This algorithm validates where the file is opening gplist.txt from
-        except FileNotFoundError:                           #if it's being opened from parent, then open from "src/plist.txt"
-            player= open("plist.txt","r+")                       #if being opened from sub then open from "plist.txt"
-            break
-        else:
-            break
-    for i in offsetline():
-        if player.readline() == (name + "\n"):
-            money = float(next(player))
-            money = money + 0.00
-            player.close()
-            return (money)
-    player.close()
 
+    player = openfile()
+    readfile = csv.reader(player)
+    for row in readfile:
+        while True:
+            try:
+                if row[0]==name:
+                    money = row[1]
+                    player.close()
+                    money= int(money)
+                    return (money)
+            except IndexError:
+                break
+            else:
+                break
+
+    player.close()
     return False
 
 
-def pbal_update(name, win):
-    name = str(name)
-    while True:
-        try:
-            player= open("src/plist.txt","r+")              #This algorithm validates where the file is opening gplist.txt from
-        except FileNotFoundError:                           #if it's being opened from parent, then open from "src/plist.txt"
-            player= open("plist.txt","r+")                       #if being opened from sub then open from "plist.txt"
-            break
-        else:
-            break
-    lnum = 0
-    for i in offsetline():
-        if player.readline() == (name + "\n"):
-            money = float(next(player))
-            money = money + win
+def pbal_update(name,win):
 
-            offsee = offsetline()
-            player.seek(offsee[lnum + 1])
+    player= openfile()
+    readfile = csv.reader(player)
 
-            player.write(str(money) + "\n")
-            player.close()
-            break
-        lnum = lnum + 1
-    return (player_cont(name))
+    rows = []
 
+    for row in readfile:
+        rows.append(row)
 
-def testing():
-    return True
+    money= player_cont(name) + win
+    player = writeinfile()
+    writer = csv.writer(player)
+    player= writeinfile()
+
+    a=0
+
+    for i in rows:
+        b = 0
+        for j in i:
+            while True:
+                try:
+                    if j==name:
+                        rows[a][b+1]=money
+                    b += 1
+                except IndexError:
+                    break
+                else:
+                    break
+
+        a += 1
+    writer.writerows(rows)
+    return(player_cont(name))
 
