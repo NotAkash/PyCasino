56a41178e6eace7b2e027762b47a7b5caed0c030
Not Akash <singhakash0412@gmail.com>
2017-11-13 19:29:58 +0530

    Added File Defaults For Future Use

--- src/__init__.py
+++ src/__init__.py
@@ -6,3 +6,20 @@ TO Fix git.push error 'Empty reply from server':
 
 """
 
+
+"""
+$List Of Players And Balances
+Name,Tokens
+
+Admin,10000
+"""
+
+
+
+
+"""
+$List Of The Games And How Much They Have Been Played
+Game, Times Played
+
+
+"""
